b4609c56d49b7f36546bb03c670560a8fb8e6b91
Rodrigo Constantino <rodrigoconstantino2@gmail.com>
2023-11-14 13:36:32 -0300

    Adicionado uma contagem dos elemetos na lista de hortFruit

--- Exercicio1.js
+++ Exercicio1.js
@@ -15,19 +15,28 @@ function variedades(item) {
     const legumes = ['cenoura', 'batata', 'brócolis', 'abóbora', 'tomate', 'alho-poró'];
     const verduras = ['alface', 'espinafre', 'rúcula', 'couve', 'agrião', 'acelga'];
 
-    return {
+    const ListaHortfruit = {
         frutas: item.filter(elementos => frutas.includes(elementos)),
         legumes: item.filter(elementos => legumes.includes(elementos)),
         verduras: item.filter(elementos => verduras.includes(elementos))
     };
+
+    const contagem = {
+        frutas: ListaHortfruit.frutas.length,
+        legumes: ListaHortfruit.legumes.length,
+        verduras: ListaHortfruit.verduras.length
+    };
+
+    return { ListaHortfruit, contagem };
 }
 
 const hotifrutis = ['banana', 'agrião', 'maçã', 'abacaxi', 'melancia', 'laranja', 'limão', 'cenoura', 'batata', 'brócolis', 'abóbora', 'tomate', 'alho-poró', 'alface', 'espinafre', 'rúcula', 'couve', 'acelga', 'maçã', 'batata'];
-const categoriasDosItems = variedades(hotifrutis);
-console.log(categoriasDosItems);
+const ListaHortfruitEContagem = variedades(hotifrutis);
+console.log(ListaHortfruitEContagem);
 
 
 // Usei function variedades dos itens, Separei cada item em sua variavel frutas, legumes, verduras, E pedi pra retornar utilizando <br/>
 // o metodo filter pra poder filtrar os elementos e ver se realmente existe, e em seguida usei o metodo includes, como o proprio nome diz <br>
-// incluir os elementos dentro das variaves, e no final só fiz a execução da chamada de cada elemento da hortfrutis.
+// incluir os elementos dentro das variaves, Adicionei uma contagem no final para mostrar quantos itens tem em cada variedade 
+
 
